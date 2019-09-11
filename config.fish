7a71b6d348e210c7667291d612996009c471928a
xecua <courser4848@gmail.com>
2019-09-11 20:02:59 +0900

    po

--- config.fish
+++ config.fish
@@ -1,36 +1,11 @@
-source ~/.config/fish/conf.d/alias.fish
-
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
 set -x XDG_RUNTIME_DIR $HOME/.tmp
 
-set -x ANYENV_ROOT $HOME/.anyenv
-set -x PATH $HOME/.anyenv/bin $PATH
-source (anyenv init -|psub)
-
-set -x PATH $HOME/.poetry/bin $PATH
-set -x PYENV_ROOT $ANYENV_ROOT/envs/pyenv
-set -x WORKON_HOME $HOME/.virtualenvs
-set -x PATH $PYENV_ROOT/bin $PATH
-
-set -x GOPATH $HOME/.golang
-set -x PATH $GOPATH/bin $PATH
-
-set -x SDKMAN_ROOT $HOME/.sdkman
-set -x KOTLIN_ROOT $SDKMAN_ROOT/candidates/kotlin/current
-set -x PATH $KOTLIN_ROOT/bin $PATH
-
-set -x PATH $HOME/.cargo/bin $PATH
-
-set -x PATH $HOME/.local/bin $PATH
-
-set -x MATLAB_USE_USERWORK 1
-set -x PATH /usr/local/MATLAB/R2019a/bin $PATH
+source $XDG_CONFIG_HOME/fish/conf.d/alias.fish
 
 set -x LANG "ja_JP.UTF-8"
 set -x LC_ALL "$LANG"
-# like pbcopy(macOS)
-alias pbcopy 'xsel --clipboard --input'
 
 # typo
 alias gti git
@@ -62,18 +37,16 @@ alias grep 'grep --color=auto'
 alias fgrep 'fgrep --color=auto'
 alias egrep 'egrep --color=auto'
 alias cls clear
-alias exp "explorer.exe ."
 
 alias vi nvim
 alias vim nvim
 alias emacs nvim
 alias :q exit
 
-# in fish, we must use like csh
+# in fish, ssh-agent must be used with -c
 alias ssh-agent 'ssh-agent -c'
 
 # oh-my-fish/theme-bobthefish
 set -g theme_display_date no
 set -g theme_display_cmd_duration no
 
-alias latexmk-docker "docker run --rm -it -v (pwd):/workdir -u (id -u):(id -g) arkark/latexmk"
