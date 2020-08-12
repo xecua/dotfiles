b24620c81fa6b5a445594b889841ff28ee74bdfd
xecua <courser4848@gmail.com>
2020-08-12 18:44:06 +0900

    locale

--- fish/conf.d/01-variable.fish
+++ fish/conf.d/01-variable.fish
@@ -5,8 +5,6 @@ set -g theme_display_cmd_duration no
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx XDG_RUNTIME_DIR $HOME/.tmp
-set -gx LANG "ja_JP.UTF-8"
-set -gx LC_ALL "$LANG"
 
 set -gx NVIM_LISTEN_ADDRESS /tmp/nvim.sock
 
