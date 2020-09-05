e7d0ed7ea4401e6b27e5f5185cb930157b4d3adb
xecua <courser4848@gmail.com>
2020-09-05 17:30:29 +0900

    やっちゃダメらしい

--- fish/conf.d/01-variable.fish
+++ fish/conf.d/01-variable.fish
@@ -3,7 +3,6 @@ set -gxp PATH $GOPATH/bin
 
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_CACHE_HOME $HOME/.cache
-set -gx XDG_RUNTIME_DIR $HOME/.tmp
 set -gx LANG "ja_JP.UTF-8"
 
 set -gx NVIM_LISTEN_ADDRESS /tmp/nvim.sock
