062d731d66ab3f2173d649c0df13af7f6148d9f6
xecua <courser4848@gmail.com>
2020-09-14 20:42:36 +0900

    po

--- fish/conf.d/11-alias.fish
+++ fish/conf.d/11-alias.fish
@@ -2,19 +2,6 @@
 alias gti git
 alias g git
 
-# use C++17, C++20 with GCC(default is C++14 in gcc6.1+)
-alias g++17 'g++ -std=c++17'
-alias g++20 'g++ -std=c++2a'
-# with gnu extensions
-alias g++17gnu 'g++ -std=gnu++17'
-alias g++20gnu 'g++ -std=gnu++2a'
-
-# use C++11,C++14,C++17,C++20 with Clang
-alias clang++11 'clang++ -std=c++11'
-alias clang++14 'clang++ -std=c++14'
-alias clang++17 'clang++ -std=c++17'
-alias clang++20 'clang++ -std=c++2a'
-
 # format style
 alias clang-format "clang-format -style='{BasedOnStyle: LLVM, BreakBeforeBraces: Stroustrup,IndentWidth: 4}'"
 
@@ -53,10 +40,8 @@ alias cls clear
 if type -q nvim
   alias vi nvim
   alias vim nvim
-  alias emacs nvim
 else
   alias vi vim
-  alias emacs vim
 end
 alias :q exit
 
@@ -70,3 +55,4 @@ end
 
 # in fish, ssh-agent must be used with -c
 alias ssh-agent 'ssh-agent -c'
+
