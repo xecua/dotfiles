1b05a1f017b2f8ac38ad9b2b986a82e57e5e9ed4
xecua <courser4848@gmail.com>
2021-01-04 18:46:17 +0900

    po

--- nvim/filetype.vim
+++ nvim/filetype.vim
@@ -2,4 +2,6 @@ augroup filetypedetect
   au! BufNewFile,BufRead */git/globalconfig setf gitconfig
   au! BufNewFile,BufRead .gitignore setf gitignore
   au! BufNewFile,BufRead *.swift setf swift
+  au! BufNewFile,BufRead *.hx setf haxe
+  au! BufNewFile,BufRead */nvim/template/* setf vim
 augroup END
---
+++ nvim/template/template.java
@@ -0,0 +1,16 @@
+:let s:file_path = expand('%:p:h')
+:let s:package_name = substitute(substitute(s:file_path, '.*src/main/java/\?', '', ''), '/', '.', 'g')
+:if s:file_path =~ '.*src/main/java/\?' && s:package_name != '' " in project
+:  %s@PACKAGE_NAME@\=s:package_name@
+:else " otherwise
+:  ,3delete " remove package declaration
+:endif
+:%s@PUBLIC_CLASS@\=expand('%:t:r')@
+package PACKAGE_NAME;
+
+public class PUBLIC_CLASS {
+    public static void main(String[] args) {
+        
+    }
+}
+
