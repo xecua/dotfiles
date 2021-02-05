dcb7cf818e37f9cf9043c125813d5dfaf64dd9eb
xecua <courser4848@gmail.com>
2021-02-05 16:04:06 +0900

    suppress error on .git directory

--- fish/conf.d/21-function.fish
+++ fish/conf.d/21-function.fish
@@ -2,8 +2,8 @@ function randomstring
   if [ -z "$argv[1]" ]
     echo "usage: randomstring width" >&2
     return 1
-  end
-  cat /dev/urandom | base64 | fold -w $argv[1] | head -1
+end
+cat /dev/urandom | base64 | fold -w $argv[1] | head -1
 end
 
 # upgrade all installed packages
@@ -17,19 +17,19 @@ end
 
 # https://qiita.com/mkeeda/items/c5fa878436f1cc957892
 function attach_tmux_session_if_needed
-    set ID (tmux list-sessions)
-    if test -z "$ID"
-        exec tmux new-session
-        return
-    end
+  set ID (tmux list-sessions)
+  if test -z "$ID"
+    exec tmux new-session
+    return
+  end
 
-    set new_session "Create New Session" 
-    set ID (echo $ID\n$new_session | peco --on-cancel=error | cut -d: -f1)
-    if test "$ID" = "$new_session"
-        exec tmux new-session
-    else if test -n "$ID"
-        exec tmux attach-session -t "$ID"
-    end
+  set new_session "Create New Session" 
+  set ID (echo $ID\n$new_session | peco --on-cancel=error | cut -d: -f1)
+  if test "$ID" = "$new_session"
+    exec tmux new-session
+  else if test -n "$ID"
+    exec tmux attach-session -t "$ID"
+  end
 end
 
 # mkdir and cd
@@ -42,21 +42,30 @@ function mkcd
   eval "cd" $argv[1]
 end
 
-# override
-function fish_title
-    # emacs' "term" is basically the only term that can't handle it.
-    if not set -q INSIDE_EMACS; or string match -vq '*,term:*' -- $INSIDE_EMACS
-      if [ (status current-command) = "fish" ]
-        if [ (git rev-parse --git-dir 2>/dev/null) ]
-          # inside git repository
-          string join '' 'fish: ' (string split '/' (git rev-parse --show-toplevel))[-1] '/' \
-                                  (string replace -ar '(\.?[^/])[^/]*/' '$1/' (string trim -rc / (git rev-parse --show-prefix)))
-        else
-          string join '' 'fish: ' (prompt_pwd)
-        end
-      else
-        status current-command
-      end
-    end
+function test-echo
+  if [ (eval "$argv[1]") ]
+    echo ok
+  else
+    echo ng
+  end
 end
 
+# override
+function fish_title
+  # emacs' "term" is basically the only term that can't handle it.
+  if not set -q INSIDE_EMACS; or string match -vq '*,term:*' -- $INSIDE_EMACS
+    if [ (status current-command) = "fish" ]
+      if [ (string match -qe '.git' (pwd)) ]
+        string join '' 'fish: ' (prompt_pwd)
+      elseif [ (git rev-parse --git-dir 2>/dev/null) ]
+        # inside git repository
+        string join '' 'fish: ' (string split '/' (git rev-parse --show-toplevel))[-1] '/' \
+        (string replace -ar '(\.?[^/])[^/]*/' '$1/' (string trim -rc / (git rev-parse --show-prefix)))
+      else
+        string join '' 'fish: ' (prompt_pwd)
+      end
+    else
+      status current-command
+    end
+  end
+end
