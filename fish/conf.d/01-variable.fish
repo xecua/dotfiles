c31fb84ea953beeff41985d093f34b29a26794fb
xecua <courser4848@gmail.com>
2021-03-03 18:11:05 +0900

    fish 3.2

--- fish/conf.d/01-variable.fish
+++ fish/conf.d/01-variable.fish
@@ -1,7 +1,4 @@
 set -gx GOPATH $HOME/.go
-set -gxp PATH $GOPATH/bin
-
-set -gxp PATH $HOME/.cargo/bin $HOME/.local/bin
 
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_CACHE_HOME $HOME/.cache
@@ -13,7 +10,12 @@ set -gx EDITOR nvim
 set -gx NVIM_LISTEN_ADDRESS /tmp/nvim.sock
 
 set -gx COURSIER_INSTALL_DIR ~/.local/share/coursier/bin
-set -gxp PATH $COURSIER_INSTALL_DIR
 
 set -gx ANDROID_SDK_ROOT /opt/android-sdk
-set -gxp PATH $ANDROID_SDK_ROOT/cmdline-tools/latest/bin
+
+fish_add_path \
+  $GOPATH/bin \
+  $HOME/.cargo/bin \
+  $HOME/.local/bin \
+  $COURSIER_INSTALL_DIR \
+  $ANDROID_SDK_ROOT/cmdline-tools/latest/bin
--- fish/conf.d/02-conditional-variable.fish
+++ fish/conf.d/02-conditional-variable.fish
@@ -3,34 +3,40 @@ if type -q sccache
 end
 
 if type -q npm
-  set -gxp PATH (npm -g bin 2>/dev/null)
+  set -l npmbin (npm -g bin 2>/dev/null)
+  if not contains $npmbin $PATH
+    fish_add_path $npmbin
+  end
 end
 
 if type -q yarn
-  set -gxp PATH (yarn global bin)
+  set -l yarnbin (yarn global bin)
+  if not contains $yarnbin $PATH
+    fish_add_path $yarnbin
+  end
 end
 
 if [ -e /usr/local/opt/gnu-sed ]
   # GNU sed installed with Homebrew
-  set -gxp PATH /usr/local/opt/gnu-sed/libexec/gnubin
+  fish_add_path /usr/local/opt/gnu-sed/libexec/gnubin
 end
 
 if [ -e /usr/local/opt/gnu-tar ]
   # GNU tar installed with Homebrew
-  set -gxp PATH /usr/local/opt/gnu-tar/libexec/gnubin
+  fish_add_path /usr/local/opt/gnu-tar/libexec/gnubin
 end
 
 if [ -e /usr/local/opt/llvm ] && status --is-interactive
   # LLVM installed with Homebrew
-  set -gxp PATH /usr/local/opt/llvm/bin
+  fish_add_path /usr/local/opt/llvm/bin
 end
 
 if [ -e /usr/local/opt/ruby ]
   # Ruby installed with Hoebrew
   # gems
-  set -gxp PATH /usr/local/lib/ruby/gems/3.0.0/bin
+  fish_add_path /usr/local/lib/ruby/gems/3.0.0/bin
   if status --is-interactive
     # interpreter
-    set -gxp PATH /usr/local/opt/ruby/bin
+    fish_add_path /usr/local/opt/ruby/bin
   end
 end
