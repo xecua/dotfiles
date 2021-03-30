daf0bcbf607c351405bc1249b609096e11aaa0f9
xecua <courser4848@gmail.com>
2021-03-30 15:53:50 +0900

    fallback

--- fish/conf.d/21-function.fish
+++ fish/conf.d/21-function.fish
@@ -18,16 +18,23 @@ end
 # https://qiita.com/mkeeda/items/c5fa878436f1cc957892
 function attach_tmux_session_if_needed
   set ID (tmux list-sessions)
-  if test -z "$ID"
+  if [ -z "$ID" ]
     exec tmux new-session
     return
   end
 
-  set new_session "Create New Session" 
-  set ID (echo $ID\n$new_session | peco --on-cancel=error | cut -d: -f1)
-  if test "$ID" = "$new_session"
+  set new_session "Create New Session"
+  if type -q fzf
+    set ID (echo $ID\n$new_session | fzf | cut -d: -f1)
+  else if type -q peco
+    set ID (echo $ID\n$new_session | peco --on-cancel=error | cut -d: -f1)
+  else
+    set ID ""
+  end
+
+  if [ "$ID" = "$new_session" ]
     exec tmux new-session
-  else if test -n "$ID"
+  else if [ -n "$ID" ]
     exec tmux attach-session -t "$ID"
   end
 end
--- fish/conf.d/99-final.fish
+++ fish/conf.d/99-final.fish
@@ -1,4 +1,7 @@
 # start tmux
-if test -z $TMUX && status --is-interactive && type -q tmux
-  attach_tmux_session_if_needed
+if [ -z $TMUX ] && status --is-interactive && type -q tmux
+  set -l ignored_terms "vscode" "iTerm.app" "Apple_Terminal"
+  if not contains $TERM_PROGRAM $ignored_terms
+    attach_tmux_session_if_needed
+  end
 end
