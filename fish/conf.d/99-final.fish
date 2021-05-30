fc864f07f86efa8d41cbd5f59eda77a36ea757cf
xecua <courser4848@gmail.com>
2021-05-30 14:36:12 +0900

    やっぱこっちの方がよさげ

--- fish/conf.d/99-final.fish
+++ fish/conf.d/99-final.fish
@@ -3,9 +3,9 @@ if [ -z $TMUX ] && status --is-interactive && type -q tmux
   set -l ignored_terms "vscode" "iTerm.app" "Apple_Terminal"
   if not contains $TERM_PROGRAM $ignored_terms
     # $TERM_PROGRAMをセットしないターミナル
-    # set -l term (ps -o command= -p (string trim (ps -o ppid= -p $fish_pid)))
-    # if not [ $term = 'tilix' ]
+    set -l term (ps -o command= -p (string trim (ps -o ppid= -p $fish_pid)))
+    if not [ $term = 'tilix' ]
       attach_tmux_session_if_needed
-    # end
+    end
   end
 end
