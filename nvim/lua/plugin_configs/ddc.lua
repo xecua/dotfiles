58b950af279a3686affd7bc10322b88bb63a0fd0
xecua <contact@caffeine.page>
2023-01-10 13:33:48 +0900

    replace

--- nvim/lua/plugin_configs/ddc.lua
+++ nvim/lua/plugin_configs/ddc.lua
@@ -47,36 +47,27 @@ vim.fn["ddc#custom#patch_filetype"]({ "ps1", "dosbatch", "autohotkey", "registry
   },
 })
 
-vim.cmd([[
-inoremap <silent><expr> <TAB>
-  \ pum#visible() ? '<Cmd>call pum#map#insert_relative(+1)<CR>' :
-  \ (col('.') <= 1 <Bar><Bar> getline('.')[col('.') - 2] =~# '\s') ?
-  \ '<TAB>' : ddc#map#manual_complete()
-inoremap <silent><expr> <S-Tab>
-  \ pum#visible() ? '<Cmd>call pum#map#insert_relative(-1)<CR>' :
-  \ '<C-h>'
-]])
--- vim.keymap.set("i", "<Tab>", function()
---   if vim.fn["pum#visible"]() == 1 then
---     return "<Cmd>call pum#map#insert_relative(+1)<CR>"
---   end
---   local _, col = unpack(vim.api.nvim_win_get_cursor(0))
---   local current_char = string.sub(vim.api.nvim_get_current_line(), col - 1, col)
---   if col <= 1 or string.match(current_char, "%s") ~= nil then
---     return "<Tab>"
---   end
---   return vim.fn["ddc#map#manual_complete"]() -- not working...?
--- end, {
---   expr = true,
---   desc = "if pum.vim visible then select next entry, otherwise insert Tab or Start completion depending on current position.",
--- })
--- vim.keymap.set("i", "<S-Tab>", function()
---   if vim.fn["pum#visible"]() == 1 then
---     return "<Cmd>call pum#map#insert_relative(-1)<CR>"
---   else
---     return "<C-h>"
---   end
--- end, { expr = true, desc = "if pum.vim visible then select previous entry, otherwise backspace" })
+vim.keymap.set("i", "<Tab>", function()
+  if vim.fn["pum#visible"]() then
+    return "<Cmd>call pum#map#insert_relative(+1)<CR>"
+  end
+  local _, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local current_char = string.sub(vim.api.nvim_get_current_line(), col - 1, col)
+  if col <= 1 or string.match(current_char, "%s") ~= nil then
+    return "<Tab>"
+  end
+  return vim.fn["ddc#map#manual_complete"]()
+end, {
+  expr = true,
+  desc = "select next entry, or insert <tab>",
+})
+vim.keymap.set("i", "<S-Tab>", function()
+  if vim.fn["pum#visible"]() then
+    return "<Cmd>call pum#map#insert_relative(-1)<CR>"
+  else
+    return "<C-h>"
+  end
+end, { expr = true, desc = "select previous entry, or delete previous character" })
 
 vim.keymap.set("i", "<C-n>", "<Cmd>call pum#map#insert_relative(+1)<CR>")
 vim.keymap.set("i", "<C-p>", "<Cmd>call pum#map#insert_relative(-1)<CR>")
