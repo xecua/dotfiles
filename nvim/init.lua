3ab14a6dbe803f2859e51569cc69163b3c27f573
xecua <contact@caffeine.page>
2023-01-15 18:26:31 +0900

    po

--- nvim/init.lua
+++ nvim/init.lua
@@ -338,6 +338,7 @@ else
 
   vim.fn["tcomment#type#Define"]("satysfi", "%% %s")
   vim.fn["tcomment#type#Define"]("glsl", "// %s")
+  vim.fn["tcomment#type#Define"]("kotlin", "// %s")
 
   vim.fn["popup_preview#enable"]()
   vim.fn["signature_help#enable"]()
