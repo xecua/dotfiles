2fa3ce3c127e8b1129c6529847a65ef06548eaf4
xecua <contact@caffeine.page>
2023-02-13 16:39:43 +0900

    enable to additional text edits with completionItem/resolve

--- nvim/lua/plugin_configs/ddc.lua
+++ nvim/lua/plugin_configs/ddc.lua
@@ -97,28 +97,62 @@ vim.keymap.set("i", "<C-y>", "<Cmd>call pum#map#confirm()<CR>")
 vim.keymap.set("i", "<C-c>", "<Cmd>call pum#map#cancel()<CR>")
 
 local ddc_group_id = vim.api.nvim_create_augroup("DdcMyCnf", { clear = true })
+
+local function apply_text_edits(edits)
+  local lnum, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  local bufnr = vim.api.nvim_get_current_buf()
+  local applying_edits = vim.tbl_filter(function(t)
+    return t.range.start.line ~= (lnum - 1)
+  end, edits)
+  vim.lsp.util.apply_text_edits(applying_edits, bufnr, "utf-8")
+end
+
 vim.api.nvim_create_autocmd("User", {
   pattern = "PumCompleteDone",
   group = ddc_group_id,
   callback = function()
     -- https://github.com/neovim/neovim/issues/12310#issuecomment-628269290 closeされたら消してよさそう?
-    -- require("notify")(vim.inspect(vim.json.decode(vim.g["pum#completed_item"].user_data.lspitem))) -- tsserverに`additionalTextEdits`があってほしいんだけど
+    -- require("notify")(vim.inspect(vim.json.decode(vim.g["pum#completed_item"].user_data.lspitem)))
     local completed = vim.g["pum#completed_item"]
     if completed == nil or completed.user_data == nil or completed.user_data.lspitem == nil then
       return
     end
 
     local lspitem = vim.json.decode(completed.user_data.lspitem)
-    if lspitem.additionalTextEdits == nil then
-      return
-    end
 
-    local lnum, _ = unpack(vim.api.nvim_win_get_cursor(0))
-    local bufnr = vim.api.nvim_get_current_buf()
-    local edits = vim.tbl_filter(function(t)
-      return t.range.start.line ~= (lnum - 1)
-    end, lspitem.additionalTextEdits)
-    vim.lsp.util.apply_text_edits(edits, bufnr, "utf-8")
+    if lspitem.additionalTextEdits ~= nil then
+      apply_text_edits(lspitem.additionalTextEdits)
+    else
+      -- try to call completionItem/resolve.
+      local method = "completionItem/resolve"
+      local method_supported = false
+      for _, client in pairs(vim.lsp.buf_get_clients(0)) do
+        if client.supports_method(method) then
+          method_supported = true
+        end
+      end
+
+      if not method_supported then
+        return
+      end
+
+      local cancel = vim.lsp.buf_request_all(0, method, lspitem, function(responses)
+        local edits = {}
+        for _, r in pairs(responses) do
+          -- require("notify")(vim.inspect(r))
+          if r.result ~= nil and r.result.additionalTextEdits ~= nil then
+            for _, e in pairs(r.result.additionalTextEdits) do
+              table.insert(edits, e)
+            end
+          end
+        end
+
+        apply_text_edits(edits)
+      end)
+      if not cancel then
+        return
+      end
+    end
   end,
   desc = "Apply additionalTextEdits if exists.",
 })
