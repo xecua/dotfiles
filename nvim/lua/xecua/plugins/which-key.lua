e9c7d57482f1207b2579aef4f43e3ba02d9c0a31
xecua <contact@caffeine.page>
2023-12-04 23:53:55 +0900

    which-key

---
+++ nvim/lua/xecua/plugins/which-key.lua
@@ -0,0 +1,26 @@
+-- lua_post_source {{{
+local wk = require('which-key')
+
+wk.setup({})
+
+-- completion source
+-- https://zenn.dev/kawarimidoll/articles/53e96110ea99e3
+wk.register({
+  ['<C-l>'] = 'Whole lines',
+  ['<C-n>'] = 'keywords in the current file',
+  ['<C-k>'] = 'keywords in dictionary',
+  ['<C-t>'] = 'keywords in thesaurus',
+  ['<C-i>'] = 'keywords in the current and included files',
+  ['<C-]>'] = 'tags',
+  ['<C-f>'] = 'file names',
+  ['<C-d>'] = 'definitions or macros',
+  ['<C-v>'] = 'Vim command-line',
+  ['<C-u>'] = 'User defined completion',
+  ['<C-o>'] = 'omni completion',
+  ['<C-s>'] = 'Spelling suggestions',
+  ['<C-z>'] = 'stop completion',
+}, {
+  mode = 'i',
+  prefix = '<C-x>',
+})
+-- }}}
