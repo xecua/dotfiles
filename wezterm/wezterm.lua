22d4b88726218704a0d42948c862ee15a6b4ae94
Shiba Shunta <contact@caffeine.page>
2024-06-09 08:23:05 +0900

    fix

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,7 +13,6 @@ config.font = wezterm.font_with_fallback({
 config.color_scheme = "Molokai"
 config.tab_bar_at_bottom = true
 config.native_macos_fullscreen_mode = true
-config.window_decorations = "RESIZE"
 config.keys = {
   { key = 'Enter', mods = 'CMD',        action = wezterm.action.ToggleFullScreen }, -- would work only for macOS
   { key = '-',     mods = 'CTRL',       action = wezterm.action.DisableDefaultAssignment },
@@ -23,13 +22,15 @@ config.keys = {
   { key = '0',     mods = 'CTRL',       action = wezterm.action.DisableDefaultAssignment },
 }
 
--- fullscreen on startup (on macOS)
-wezterm.on('gui-startup', function(cmd)
-  local _, _, window = wezterm.mux.spawn_window(cmd or {})
-  if utils.get_os() == "Darwin" then
+if utils.get_os() == "Darwin" then
+  config.window_decorations = "RESIZE"
+  -- fullscreen on startup (on macOS)
+  wezterm.on('gui-startup', function(cmd)
+    local _, _, window = wezterm.mux.spawn_window(cmd or {})
     window:gui_window():toggle_fullscreen()
-  end
-end)
+  end)
+end
+
 
 local ok, local_config = pcall(require, 'local')
 if ok then
