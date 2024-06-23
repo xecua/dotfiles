39c214f9431b8340429a430f22beb298e5164f63
Shiba Shunta <contact@caffeine.page>
2024-06-23 15:25:51 +0900

    fix

--- nvim/lua/xecua/plugins/fugitive.lua
+++
@@ -1,6 +0,0 @@
--- lua_add {{{
-vim.keymap.set('n', '<Leader>gb', '<Cmd>Git blame<CR>')
-vim.keymap.set('n', '<Leader>gl', '<Cmd>Git log -p %<CR>')
-vim.keymap.set('n', '<Leader>gs', '<Cmd>Git<CR>')
-vim.keymap.set('n', '<Leader>gd', '<Cmd>Gdiffsplit<CR>')
--- }}}
