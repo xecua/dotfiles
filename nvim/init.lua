a61e7dc0ad6558fe65f17aa2cf90c1d41c725fea
Shiba Shunta <contact@caffeine.page>
2024-06-30 20:41:48 +0900

    split filetype

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,26 +12,6 @@ if vim.fn.isdirectory('/opt/homebrew/opt/fzf') == 1 then
   vim.opt.rtp:append('/opt/homebrew/opt/fzf')
 end
 
--- filetype
-vim.filetype.add({
-  extension = {
-    tsx = 'typescriptreact',
-    jsx = 'typescriptreact',
-    er = 'python', -- erg
-    hx = 'haxe',
-    frag = 'glsl',
-    vert = 'glsl',
-  },
-  pattern = {
-    ['.*/git/config.*'] = { 'gitconfig', { priority = 10 } },
-    -- [".*/git/ignore.*"] = { "gitignore", { priority = 10 } }, -- cause error?
-    ['%.gitconfig.*'] = { 'gitconfig', { priority = 10 } },
-    ['.*/git/attributes.*'] = { 'gitattributes', { priority = 10 } },
-    ['.*/nvim/template/.*'] = { 'vim', { priority = 10 } },
-    ['.textlintrc'] = { 'json', { priority = 10 } },
-  },
-})
-
 -- :h DiffOrig Luaで書き換える?
 vim.api.nvim_create_user_command(
   'DiffOrig',
