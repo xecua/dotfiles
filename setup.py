c160e29851c9cb3486340128437cb11abf5de507
Shiba Shunta <contact@caffeine.page>
2024-07-10 23:10:23 +0900

    hypr

--- setup.py
+++ setup.py
@@ -17,9 +17,12 @@ data_home = Path(os.getenv("XDG_DATA_HOME", home / '.local' / 'share'))
 uname = os.uname().sysname
 
 
-def make_symlink(link: Path, target: Path):
+def make_symlink(link: Path, target: Path | None = None):
     if not link.parent.exists():
         link.parent.mkdir(parents=True, exist_ok=True)
+
+    if target is None:
+        target = script_dir / link.name
 
     if not link.exists():
         link.symlink_to(target, target_is_directory=True)
@@ -32,30 +35,31 @@ make_symlink(config_home / 'fish' / 'completions',
              script_dir / 'fish' / 'completions')
 make_symlink(config_home / 'fish' / 'conf.d', script_dir / 'fish' / 'conf.d')
 
-make_symlink(home / '.profile', script_dir / '.profile')
-make_symlink(home / '.bashrc', script_dir / '.bashrc')
-make_symlink(home / '.bash_profile', script_dir / '.bash_profile')
-make_symlink(home / '.zprofile', script_dir / '.zprofile')
-make_symlink(home / '.zshrc', script_dir / '.zshrc')
+make_symlink(home / '.profile')
+make_symlink(home / '.bashrc')
+make_symlink(home / '.bash_profile')
+make_symlink(home / '.zprofile')
+make_symlink(home / '.zshrc')
 
-make_symlink(home / '.mutagen.yml', script_dir / '.mutagen.yml')
-make_symlink(home / '.clang-format', script_dir / '.clang-format')
+make_symlink(home / '.mutagen.yml')
+make_symlink(home / '.clang-format')
 
-make_symlink(config_home / 'broot', script_dir / 'broot')
-make_symlink(config_home / 'nvim', script_dir / 'nvim')
-make_symlink(config_home / 'ideavim', script_dir / 'ideavim')
-make_symlink(config_home / 'git', script_dir / 'git')
+make_symlink(config_home / 'broot')
+make_symlink(config_home / 'nvim')
+make_symlink(config_home / 'ideavim')
+make_symlink(config_home / 'git')
 make_symlink(config_home / 'lazygit' / 'config.yml', script_dir / 'lazygit' / 'config.yml')
-make_symlink(config_home / 'fd', script_dir / 'fd')
-make_symlink(config_home / 'picom', script_dir / 'picom')
-make_symlink(config_home / 'ripgrep', script_dir / 'ripgrep')
-make_symlink(config_home / 'i3', script_dir / 'i3')
-make_symlink(config_home / 'i3status-rust', script_dir / 'i3status-rust')
-make_symlink(config_home / 'dunst', script_dir / 'dunst')
-make_symlink(config_home / 'sway', script_dir / 'sway')
-make_symlink(config_home / 'swaylock', script_dir / 'swaylock')
-make_symlink(config_home / 'hypr', script_dir / 'hypr')
-make_symlink(config_home / 'starship.toml', script_dir / 'starship.toml')
+make_symlink(config_home / 'fd')
+make_symlink(config_home / 'picom')
+make_symlink(config_home / 'ripgrep')
+make_symlink(config_home / 'i3')
+make_symlink(config_home / 'i3status-rust')
+make_symlink(config_home / 'i3bar-river')
+make_symlink(config_home / 'dunst')
+make_symlink(config_home / 'sway')
+make_symlink(config_home / 'swaylock')
+make_symlink(config_home / 'hypr')
+make_symlink(config_home / 'starship.toml')
 
 make_symlink(home / '.satysfi' / 'local' / 'packages',
              script_dir / 'satysfi' / 'packages')
@@ -63,23 +67,23 @@ make_symlink(config_home / 'latexmk' / 'latexmkrc',
              script_dir / 'latexmk' / 'latexmkrc')
 make_symlink(config_home / 'tmux' / 'tmux.conf',
              script_dir / 'tmux' / 'tmux.conf')
-make_symlink(config_home / 'wezterm', script_dir / 'wezterm')
+make_symlink(config_home / 'wezterm')
 
 make_symlink(config_home / 'ranger' / 'rc.conf',
              script_dir / 'ranger' / 'rc.conf')
 make_symlink(config_home / 'ranger' / 'rifle.conf',
              script_dir / 'ranger' / 'rifle.conf')
 
-make_symlink(config_home / 'alacritty', script_dir / 'alacritty')
+make_symlink(config_home / 'alacritty')
 if not (home / '.alacritty.local.toml').exists():
     # create
     (home / '.alacritty.local.toml').touch()
 
 # Linux only
-make_symlink(config_home / 'xremap', script_dir / 'xremap')
-make_symlink(config_home / 'kanshi', script_dir / 'kanshi')
-make_symlink(config_home / 'fontconfig', script_dir / 'fontconfig')
-make_symlink(config_home / 'zathura', script_dir / 'zathura')
+make_symlink(config_home / 'xremap')
+make_symlink(config_home / 'kanshi')
+make_symlink(config_home / 'fontconfig')
+make_symlink(config_home / 'zathura')
 make_symlink(config_home / 'libskk' / 'rules',
              script_dir / 'skk' / 'libskk' / 'rules')
 
@@ -126,10 +130,3 @@ try:
 except:
     print("npm does not exist.",
           "Please setup manually or run this script again.")
-
-# for yarn berry?
-# yarn_check = subprocess.run(['yarn', '-v'],
-#                             stdout=subprocess.DEVNULL,
-#                             stderr=subprocess.DEVNULL)
-# if yarn_check.returncode == 0:
-#     subprocess.run(['yarn', 'set', 'prefix', data_home / 'yarn'])
