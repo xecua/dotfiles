d2c6bfee2a7ca5657152c9e16953772a63bed4b9
Shiba Shunta <contact@caffeine.page>
2024-10-18 00:02:12 +0900

    improve injection query

--- nvim/queries/toml/injections.scm
+++ nvim/queries/toml/injections.scm
@@ -2,6 +2,7 @@
   (#set! injection.language "comment")
 )
 
+; #gsub!とか使ってまとめられないかなあ
 (pair
   (bare_key) @bare_key
   (#any-of? @bare_key
@@ -11,6 +12,22 @@
     "hook_post_update"
     "hook_source")
   (string) @injection.content
+  (#match? @injection.content "^\"\"\"")
+  (#offset! @injection.content 0 3 0 -3)
+  (#set! injection.language "vim")
+)
+
+(pair
+  (bare_key) @bare_key
+  (#any-of? @bare_key
+    "hook_add"
+    "hook_done_update"
+    "hook_post_source"
+    "hook_post_update"
+    "hook_source")
+  (string) @injection.content
+  (#match? @injection.content "^\"[^\"]")
+  (#offset! @injection.content 0 1 0 -1)
   (#set! injection.language "vim")
 )
 
@@ -23,5 +40,21 @@
     "lua_post_update"
     "lua_source")
   (string) @injection.content
+  (#match? @injection.content "^\"\"\"")
+  (#offset! @injection.content 0 3 0 -3)
+  (#set! injection.language "lua")
+)
+
+(pair
+  (bare_key) @bare_key
+  (#any-of? @bare_key
+    "lua_add"
+    "lua_done_update"
+    "lua_post_source"
+    "lua_post_update"
+    "lua_source")
+  (string) @injection.content
+  (#match? @injection.content "^\"[^\"]")
+  (#offset! @injection.content 0 1 0 -1)
   (#set! injection.language "lua")
 )
