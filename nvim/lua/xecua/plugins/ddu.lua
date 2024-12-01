c6a1d023d14e7717947c32032d800efbf4503778
Shiba Shunta <contact@caffeine.page>
2024-12-01 17:39:34 +0900

    ddu

--- nvim/lua/xecua/plugins/ddu.lua
+++ nvim/lua/xecua/plugins/ddu.lua
@@ -2,7 +2,6 @@
 -- vim.g.loaded_ddu_rg = 1 -- prevent command definition by plugin
 
 vim.api.nvim_create_user_command("DduRgLive", function()
-    -- actionOptionsでquitをfalseにすれば閉じなくなる(Filer参照)が、openでwincmdを噛ませるように変更する必要がある
     vim.fn["ddu#start"]({
         sources = { { name = "rg", options = { volatile = true, matchers = {} } } },
         uiParams = { ff = { ignoreEmpty = false } },
@@ -31,7 +30,6 @@ vim.api.nvim_create_user_command("DduFiler", function()
     vim.fn["ddu#start"]({
         ui = "filer",
         sources = { { name = "file" } },
-        sourceOptions = { file = { columns = { "icon_filename" } } },
         actionOptions = {
             narrow = { quit = false },
             open = { quit = false },
@@ -47,101 +45,23 @@ vim.api.nvim_create_autocmd("FileType", {
     callback = function()
         local opts = { buffer = true, silent = true }
         vim.keymap.set("n", "<CR>", "<Cmd>call ddu#ui#do_action('itemAction')<CR>", opts)
+        -- function()
+        --     local item = vim.fn["ddu#ui#get_item"]()
+        --     if item["__sourceName"] == "rg" then
+        --       -- wincmd pだとpreviewに行ってしまうのでダメ
+        --     end
+        --     vim.fn["ddu#ui#do_action"]("itemAction")
+        -- end, opts)
         vim.keymap.set("n", "a", "<Cmd>call ddu#ui#do_action('chooseAction')<CR>", opts)
         vim.keymap.set("n", "/", "<Cmd>call ddu#ui#do_action('openFilterWindow')<CR>", opts)
-        vim.keymap.set("n", "p", "<Cmd>call ddu#ui#do_action('togglePreview')<CR>", opts)
         vim.keymap.set("n", ",", "<Cmd>call ddu#ui#do_action('toggleSelectItem')<CR>", opts)
         vim.keymap.set("n", "t", "<Cmd>call ddu#ui#do_action('toggleAllItems')<CR>", opts)
         vim.keymap.set("n", "f", "<Cmd>call ddu#ui#do_action('itemAction', #{ name: 'quickfix' })<CR>", opts)
         vim.keymap.set("n", "h", "<Cmd>call ddu#ui#do_action('collapseItem')<CR>", opts)
         vim.keymap.set("n", "l", "<Cmd>call ddu#ui#do_action('expandItem')<CR>", opts)
         vim.keymap.set("n", "q", "<Cmd>call ddu#ui#do_action('quit')<CR>", opts)
-
-        local converter_display_word = "converter_display_word"
-        -- _はマーカーとして使う。実際には各sourceのオプションに反映しないといけない
-        vim.keymap.set("n", "w", function()
-            local current = vim.fn["ddu#custom#get_current"]()
-            local new_source_options = current.sourceOptions
-            local matchers = new_source_options._.matchers
-
-            -- converter_display_wordは最初にいるはず
-            if matchers[1] ~= converter_display_word then
-                vim.notify("Display word included.")
-                table.insert(matchers, 1, converter_display_word)
-            else
-                vim.notify("Display word excluded.")
-                table.remove(matchers, 1)
-            end
-            for _, source in ipairs(current.sources) do
-                -- 変更されているはずなのでそのままコピー。
-                new_source_options[source.name].matchers = matchers
-            end
-
-            vim.fn["ddu#ui#do_action"]("updateOptions", { sourceOptions = new_source_options })
-            vim.fn["ddu#redraw"](vim.b.ddu_ui_name, { method = "refreshItems" })
-        end, opts)
-
-        local matcher_fzf = "matcher_fzf"
-        local matcher_substring = "matcher_substring"
-        local matcher_regex = "matcher_multi_regex"
-        local sorter_alpha = "sorter_alpha"
-        local sorter_fzf = "sorter_fzf"
-        vim.keymap.set("n", "m", function()
-            -- switch matching mode
-            local current = vim.fn["ddu#custom#get_current"]()
-            local new_source_options = current.sourceOptions
-            local matchers = new_source_options._.matchers
-            local sorters = new_source_options._.sorters
-
-            -- converter_display_wordがある可能性がある
-            for i, m in ipairs(matchers) do
-                if m == matcher_fzf then
-                    vim.notify("Changing matcher to substring.")
-                    matchers[i] = matcher_substring
-                    sorters = { sorter_alpha }
-                    break
-                elseif m == matcher_substring then
-                    vim.notify("Changing matcher to regex.")
-                    matchers[i] = matcher_regex
-                    break
-                elseif m == matcher_regex then
-                    vim.notify("Changing matcher to fuzzy.")
-                    matchers[i] = matcher_fzf
-                    sorters = { sorter_fzf }
-                    break
-                end
-            end
-
-            for _, source in ipairs(current.sources) do
-                new_source_options[source.name].matchers = matchers
-                new_source_options[source.name].sorters = sorters
-            end
-
-            vim.fn["ddu#ui#do_action"]("updateOptions", { sourceOptions = new_source_options })
-
-            vim.fn["ddu#redraw"](vim.b.ddu_ui_name, { method = "refreshItems" })
-        end, opts)
     end,
 })
--- vim.api.nvim_create_autocmd('VimResized', {
---   group = ddu_group_id,
---   callback = function()
---     vim.fn['ddu#custom#patch_global']({
---       uiParams = {
---         ff = {
---           winCol = vim.o.columns / 4,
---           winRow = vim.o.lines / 8,
---           winWidth = vim.o.columns / 2,
---           winHeight = math.floor(vim.o.lines / 2) - 2,
---           previewCol = vim.o.columns / 4,
---           previewRow = vim.o.lines * 3 / 4 + 1,
---           previewWidth = vim.o.columns / 2,
---           previewHeight = math.floor(vim.o.lines / 8),
---         },
---       },
---     })
---   end,
--- })
 vim.api.nvim_create_autocmd("User", {
     pattern = "Ddu:ui:ff:openFilterWindow",
     command = 'call pum#set_option("reversed", v:true)',
@@ -169,26 +89,38 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.keymap.set("n", "p", "<Cmd>call ddu#ui#do_action('itemAction', #{name: 'paste' })<CR>", opts)
 
         vim.keymap.set("n", "s", function()
-            local isTree = vim.fn["ddu#ui#get_item"]()["isTree"] or false
-            if not isTree then
+            if not vim.fn["ddu#ui#get_item"]()["isTree"] then
                 vim.fn["ddu#ui#do_action"](
                     "itemAction",
                     { name = "open", params = { command = "wincmd p|wincmd s|drop" } }
                 )
             end
-        end)
+        end, opts)
         vim.keymap.set("n", "v", function()
-            local isTree = vim.fn["ddu#ui#get_item"]()["isTree"] or false
-            if not isTree then
+            if not vim.fn["ddu#ui#get_item"]()["isTree"] then
                 vim.fn["ddu#ui#do_action"](
                     "itemAction",
                     { name = "open", params = { command = "wincmd p|wincmd v|drop" } }
                 )
             end
-        end)
+        end, opts)
+        vim.keymap.set("n", "t", function()
+            if not vim.fn["ddu#ui#get_item"]()["isTree"] then
+                vim.fn["ddu#ui#do_action"]("itemAction", { name = "open", params = { command = "tab drop" } })
+            end
+        end, opts)
         vim.keymap.set("n", "l", function()
-            local isTree = vim.fn["ddu#ui#get_item"]()["isTree"] or false
-            if isTree then
+            if vim.fn["ddu#ui#get_item"]()["isTree"] then
+                vim.fn["ddu#ui#do_action"]("expandItem", { isInTree = true })
+            else
+                vim.fn["ddu#ui#do_action"]("itemAction", { name = "open", params = { command = "wincmd p|drop" } })
+            end
+        end, opts)
+        vim.keymap.set("n", "<LeftRelease>", function()
+            local item = vim.fn["ddu#ui#get_item"]()
+            if item["isTree"] and item["__expanded"] then
+                vim.fn["ddu#ui#do_action"]("collapseItem")
+            elseif item["isTree"] then
                 vim.fn["ddu#ui#do_action"]("expandItem", { isInTree = true })
             else
                 vim.fn["ddu#ui#do_action"]("itemAction", { name = "open", params = { command = "wincmd p|drop" } })
@@ -196,8 +128,7 @@ vim.api.nvim_create_autocmd("FileType", {
         end, opts)
 
         vim.keymap.set("n", "<CR>", function()
-            local isTree = vim.fn["ddu#ui#get_item"]()["isTree"] or false
-            if isTree then
+            if vim.fn["ddu#ui#get_item"]()["isTree"] then
                 vim.fn["ddu#ui#do_action"]("itemAction", { name = "narrow" })
             else
                 vim.fn["ddu#ui#do_action"]("itemAction", { name = "open", params = { command = "wincmd p|drop" } })
@@ -240,18 +171,6 @@ vim.fn["ddu#custom#patch_global"]({
         ff = {
             previewWidth = 80,
             previewSplit = "vertical",
-            -- split = 'floating',
-            -- floatingBorder = 'rounded',
-            -- previewFloating = true,
-            -- previewFloatingBorder = 'rounded',
-            -- winCol = vim.o.columns / 4,
-            -- winRow = vim.o.lines / 8,
-            -- winWidth = vim.o.columns / 2,
-            -- winHeight = math.floor(vim.o.lines / 2) - 2,
-            -- previewCol = vim.o.columns / 4,
-            -- previewRow = vim.o.lines * 3 / 4 + 1,
-            -- previewWidth = vim.o.columns / 2,
-            -- previewHeight = math.floor(vim.o.lines / 8),
             startAutoAction = true,
             statusline = false,
             autoAction = { name = "preview", sync = false },
@@ -270,7 +189,11 @@ vim.fn["ddu#custom#patch_global"]({
     sourceOptions = {
         _ = { matchers = { "matcher_fzf" }, sorters = { "sorter_fzf" } },
         source = { defaultAction = "execute" },
-        file = { matchers = { "matcher_hidden" }, sorters = {} }, -- filerでしか使ってない
+        file = { -- filerでしか使ってないのでそれ用に調整してしまう
+            columns = { "icon_filename" },
+            matchers = { "matcher_hidden" },
+            sorters = { "sorter_alpha" },
+        },
         lsp_documentSymbol = { converters = { "converter_lsp_symbol" } },
         lsp_workspaceSymbol = { converters = { "converter_lsp_symbol" } },
     },
