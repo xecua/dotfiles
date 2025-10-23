eb53aba5c940c191cb7efdfeaff798cafecd12c8
Shiba Shunta <contact@caffeine.page>
2025-10-23 14:19:01 +0000

    a

--- nvim/filetype.lua
+++ nvim/filetype.lua
@@ -1,20 +1,11 @@
 vim.filetype.add({
     extension = {
-        tsx = "typescriptreact",
-        jsx = "typescriptreact",
         er = "python", -- erg
         hx = "haxe",
         mdc = "markdown",
-        frag = "glsl",
-        vert = "glsl",
     },
     pattern = {
-        [".*/git/config.*"] = "gitconfig",
-        [".*/git/ignore.*"] = "gitignore", -- cause error?
-        ["%.gitconfig.*"] = "gitconfig",
-        [".*/git/attributes.*"] = "gitattributes",
         [".textlintrc"] = "json",
-        [".*/hypr/.*%.conf"] = "hyprlang",
         ["docker%-compose%.ya?ml"] = "yaml.docker-compose",
         ["compose%.ya?ml"] = "yaml.docker-compose",
         ["openapi.*%.ya?ml"] = "yaml.openapi",
