fe0edc48091ec90b564573157328302a5ae6d1d5
Shiba Shunta <contact@caffeine.page>
2025-11-21 23:46:47 +0900

    a

--- fish/conf.d/11-alias.fish
+++ fish/conf.d/11-alias.fish
@@ -1,70 +1,78 @@
 # idiomatic, non-modification-prone commands
 if status is-interactive
-  alias gitop='cd (git rev-parse --show-toplevel)'
+    alias gitop='cd (git rev-parse --show-toplevel)'
 
-  alias :q=exit
-  alias nvimdiff='nvim -d'
+    alias :q=exit
+    alias nvimdiff='nvim -d'
 
-  if type -q trash
-    alias rm=trash
-  else if type -q gio
-    alias rm="gio trash"
-  end
-
-  if not type -q sudoedit
-    alias sudoedit='sudo -e'
-  end
-
-  if type -q eza # actively maintained fork
-    set -l eza_command 'eza --header --git --time-style=iso --modified --created'
-    alias l="$eza_command"
-    alias ls="$eza_command --icons"
-    alias ll="$eza_command --icons --long"
-    alias la="$eza_command --icons --long --all"
-    alias lt="$eza_command --icons --tree --git-ignore"
-    alias llt="$eza_command --icons --tree --long"
-    alias lat="$eza_command --icons --tree --long --all"
-  else
-    alias l='ls -C'
-    alias ll='ls -alF'
-    alias la='ls -A'
-
-    if type -q tree
-      # treeコマンドを入れてみて似たような感じにする
-      alias lt="tree"
-      alias llt="tree"
-      alias lat="tree -a"
+    if type -q trash
+        alias rm=trash
+    else if type -q gio
+        alias rm="gio trash"
     end
-  end
 
-  if type -q btm
-    alias top="btm -b"
-  else if type -q htop
-    alias top=htop
-  end
+    if not type -q sudoedit
+        alias sudoedit='sudo -e'
+    end
 
-  if type -q batman
-    alias man=batman
-  end
+    if type -q eza # actively maintained fork
+        set -l eza_command 'eza --header --git --time-style=iso --modified --created'
+        alias l="$eza_command"
+        alias ls="$eza_command --icons"
+        alias ll="$eza_command --icons --long"
+        alias la="$eza_command --icons --long --all"
+        alias lt="$eza_command --icons --tree --git-ignore"
+        alias llt="$eza_command --icons --tree --long"
+        alias lat="$eza_command --icons --tree --long --all"
+    else
+        alias l='ls -C'
+        alias ll='ls -alF'
+        alias la='ls -A'
 
-  if type -q gsed
-    alias sed=gsed
-  end
+        if type -q tree
+            # treeコマンドを入れてみて似たような感じにする
+            alias lt="tree"
+            alias llt="tree"
+            alias lat="tree -a"
+        end
+    end
 
-  if type -q gtar
-    alias tar=gtar
-  end
+    if type -q btm
+        alias top="btm -b"
+    else if type -q htop
+        alias top=htop
+    end
 
-  if type -q gawk
-    alias awk=gawk
-  end
+    if type -q batman
+        alias man=batman
+    end
 
-  if type -q lazygit
-    alias lg=lazygit
-  end
+    if type -q gsed
+        alias sed=gsed
+    end
 
-  alias avante='nvim -c "lua vim.defer_fn(function()require(\"avante.api\").zen_mode()end, 100)"'
+    if type -q gtar
+        alias tar=gtar
+    end
 
-  alias cbcopy=fish_clipboard_copy
-  alias cbpaste=fish_clipboard_paste
+    if type -q gawk
+        alias awk=gawk
+    end
+
+    if type -q lazygit
+        alias lg=lazygit
+    end
+
+    # Workaround for https://github.com/flutter/flutter/issues/59430
+    if type -q flutter
+        alias flutter='XDG_CONFIG_HOME=$XDG_CONFIG_HOME/flutter command flutter'
+    end
+    if type -q fvm
+        alias fvm='XDG_CONFIG_HOME=$XDG_CONFIG_HOME/fvm command fvm'
+    end
+
+    alias avante='nvim -c "lua vim.defer_fn(function()require(\"avante.api\").zen_mode()end, 100)"'
+
+    alias cbcopy=fish_clipboard_copy
+    alias cbpaste=fish_clipboard_paste
 end
