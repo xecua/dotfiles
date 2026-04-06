22fb57a93fcb532ed43b4208c86f061e6d3d98fe
Shiba Shunta <contact@caffeine.page>
2026-04-06 22:54:05 +0900

    po

--- fish/conf.d/10-env.fish
+++ fish/conf.d/10-env.fish
@@ -3,4 +3,8 @@ if status is-interactive
     if set -q TMUX
         set -gx FZF_DEFAULT_OPTS "--tmux 80%"
     end
+
+    if type -q podman && test (uname) = Darwin
+        set -x PODMAN_HOST "unix://"(podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}')
+    end
 end
